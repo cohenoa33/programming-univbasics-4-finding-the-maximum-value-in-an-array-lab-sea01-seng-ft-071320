9ad1ab6e2e3a44cc5f7322a8d83f12e407ab2dcb
Noa Rabin-Cohen <cohenoa33@gmail.com>
2020-06-29 18:58:44 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -2,6 +2,8 @@ def find_max_value(array)
   # Add your solution here
   counter = 0 
   while counter < array.length do 
-    
+    array_new = array.sort 
+    array_new.reverse!
+    return array_new[0]
   end
 end
